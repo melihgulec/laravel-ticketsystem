969e539664d0d10f4a3abb9195193c37ed42eefc
Melih Güleç <melih.gulec14@gmail.com>
2023-02-23 02:22:45 +0300

    Create ticket relations

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Ticket extends Model
 {
     use HasFactory;
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
+
+    public  function category(){
+        return $this->belongsTo(Category::class);
+    }
 }
