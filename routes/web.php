29f5e08cbeeae6f00700050219a330c1dcae1232
Melih Güleç <melih.gulec14@gmail.com>
2023-02-23 03:07:42 +0300

    Add routes and deleted welcome page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\SessionController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [SessionController::class, 'create']);
+Route::post('/', [SessionController::class, 'store']);
