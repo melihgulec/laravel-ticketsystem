a8e5889b966d2e464b758c9b7770675b5564faee
Melih Güleç <melih.gulec14@gmail.com>
2023-02-23 03:08:41 +0300

    Add password attribute to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,6 +42,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public function setPasswordAttribute($password){
+        $this->attributes['password'] = bcrypt($password);
+    }
+
     public function role(){
         return $this->belongsTo(Role::class);
     }
