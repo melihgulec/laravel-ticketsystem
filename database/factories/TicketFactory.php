7143f0e17c2b6f328c2e0dfce02f3baee79395ff
Melih Güleç <melih.gulec14@gmail.com>
2023-02-24 18:50:35 +0300

    Create Ticket, product and category factory

---
+++ database/factories/TicketFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Ticket>
+ */
+class TicketFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'product_id' => Product::factory(),
+            'category_id' => Category::factory(),
+            'title' => $this->faker->word,
+            'explanation' => $this->faker->paragraph(),
+            'status' => $this->faker->boolean,
+            'attachments' => '',
+        ];
+    }
+}
