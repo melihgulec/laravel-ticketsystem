b0a0ba2894ecc0825bdb6cee9c4b53095beaf71b
Melih Güleç <melih.gulec14@gmail.com>
2023-02-25 03:56:43 +0300

    Create create-ticket-float-button component

---
+++ resources/views/components/create-ticket-float-button.blade.php
@@ -0,0 +1,12 @@
+<a href="/tickets/create">
+    <div class="fixed bottom-3 right-2 bg-blue-500 text-white p-3 rounded-xl">
+        <div class="flex flex-row items-center">
+            <span class="font-bold text-lg">
+                +
+            </span>
+            <span class="ml-3">
+                Create Ticket
+            </span>
+        </div>
+    </div>
+</a>
