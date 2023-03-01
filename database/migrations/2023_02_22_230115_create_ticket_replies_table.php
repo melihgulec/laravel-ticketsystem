19a824192b301b763c5a1145c013c73c125d40c4
Melih Güleç <melih.gulec14@gmail.com>
2023-03-01 19:48:36 +0300

    Add soft deletes to ticket reply

--- database/migrations/2023_02_22_230115_create_ticket_replies_table.php
+++ database/migrations/2023_02_22_230115_create_ticket_replies_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->text("explanation");
             $table->text("attachments");
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
