78a50bfb52f2fc0306704f3c0c7a7fb6962a5b5e
Melih Güleç <melih.gulec14@gmail.com>
2023-03-03 15:35:51 +0300

    Add font awesome to layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script src="https://kit.fontawesome.com/b6283481d8.js" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body class="h-screen bg-gray-100">
