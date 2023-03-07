91cd4993c6bd71704d5d5ba74fb2085d1937aec4
Melih Güleç <melih.gulec14@gmail.com>
2023-03-07 15:02:26 +0300

    Add role_id foreign key to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('role_id');
             $table->string('name');
             $table->string('username')->unique();
             $table->string('email')->unique();
