1a71c8d0a35f590f0155ce85e50628c379496aa2
Melih Güleç <melih.gulec14@gmail.com>
2023-03-08 16:00:07 +0300

    Add livewire styles and scripts to layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,11 +8,13 @@
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <script src="https://kit.fontawesome.com/b6283481d8.js" crossorigin="anonymous"></script>
+    @livewireStyles
     <title>Document</title>
 </head>
 <body class="h-screen bg-gray-100">
     {{ $slot }}
 
     <x-flash />
+    @livewireScripts
 </body>
 </html>
