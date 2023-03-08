af4d961f34c05879e34cac0986b501867e8e8776
Melih Güleç <melih.gulec14@gmail.com>
2023-03-08 16:02:27 +0300

    Edit ticket-status component for new types

--- resources/views/components/ticket-status.blade.php
+++ resources/views/components/ticket-status.blade.php
@@ -1,6 +1,16 @@
 @props(['type'])
-<div class="rounded px-6 py-2 text-center w-24 text-white {{ $type == 0 ? 'bg-red-500' : 'bg-green-500'}}">
+<div class="rounded px-6 py-2 text-center w-24 text-white
+{{
+    ($type == 1 ? "bg-green-500" :
+    ($type == 2 ? "bg-blue-500" :
+    ($type == 3 ? "bg-red-500": '')))
+}}
+">
     <span class="font-bold">
-        {{ $type == 0 ? 'Closed' : 'Open' }}
+        {{
+    ($type == 1 ? "Open" :
+    ($type == 2 ? "Replied" :
+    ($type == 3 ? "Closed": "")))
+}}
     </span>
 </div>
