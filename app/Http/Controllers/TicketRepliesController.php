557d531c609d9a63fc5052e89978ef7cd35eb033
Melih Güleç <melih.gulec14@gmail.com>
2023-03-08 16:06:56 +0300

    Added the feature to make the status of the ticket replied when staff responds to the ticket

--- app/Http/Controllers/TicketRepliesController.php
+++ app/Http/Controllers/TicketRepliesController.php
@@ -11,18 +11,24 @@ class TicketRepliesController extends Controller
         $attributes = request()->validate([
             'reply' => ['required', 'min:5']
         ]);
+
         $ticket->replies()->create([
             'user_id' => request()->user()->id,
             'explanation' => $attributes['reply'],
             'attachments' => '',
         ]);
 
+        if(request()->user()->can('staff')){
+            $ticket->update([
+                'status_id' => 2
+            ]);
+        }
+
         return back()->with('dialogMessage', 'Your ticket reply has been created successfully!');
     }
 
     public function destroy(Ticket $ticket, TicketReply $reply){
         TicketReply::where('id', '=', $reply->id, 'and', 'ticket_id', '=', $ticket->id)->delete();
-
         return back()->with('dialogMessage', 'Your reply has been deleted successfully!');
     }
 }
