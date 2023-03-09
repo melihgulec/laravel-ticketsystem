2aace013aa5d4f79f57f768e7fbb13fa4882ca8d
Melih Güleç <melih.gulec14@gmail.com>
2023-03-09 14:54:45 +0300

    Add jQuery to layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,6 +8,10 @@
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <script src="https://kit.fontawesome.com/b6283481d8.js" crossorigin="anonymous"></script>
+    <script
+        src="https://code.jquery.com/jquery-3.6.4.slim.min.js"
+        integrity="sha256-a2yjHM4jnF9f54xUQakjZGaqYs/V1CYvWpoqZzC2/Bw="
+        crossorigin="anonymous"></script>
     @livewireStyles
     <title>Document</title>
 </head>
