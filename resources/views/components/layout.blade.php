eaa4c0afc7938279e4fde5f225871f5a68406cb5
Melih Güleç <melih.gulec14@gmail.com>
2023-03-14 18:40:44 +0300

    Delete google font links in layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,20 +8,15 @@
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <script src="https://kit.fontawesome.com/b6283481d8.js" crossorigin="anonymous"></script>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <script
         src="https://code.jquery.com/jquery-3.6.4.slim.min.js"
         integrity="sha256-a2yjHM4jnF9f54xUQakjZGaqYs/V1CYvWpoqZzC2/Bw="
         crossorigin="anonymous"></script>
-    <link rel="stylesheet" href='{{ resource_path('css/app.css') }}' />
     @livewireStyles
     <title>Document</title>
 </head>
-<body class="h-screen bg-gray-100">
+<body class="h-screen bg-gray-100 text-sm">
     {{ $slot }}
-
     <x-flash />
     @livewireScripts
 </body>
