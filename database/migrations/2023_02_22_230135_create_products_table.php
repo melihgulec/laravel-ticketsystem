f4d664c7cddd03e7989f8fc0d0a601bb5d80c954
Melih Güleç <melih.gulec14@gmail.com>
2023-03-20 18:42:41 +0300

    Add product category id foregin key to products table

--- database/migrations/2023_02_22_230135_create_products_table.php
+++ database/migrations/2023_02_22_230135_create_products_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId("product_category_id");
             $table->string("name");
             $table->timestamps();
         });
