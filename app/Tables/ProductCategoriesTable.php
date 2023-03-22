1adc0edf3bddeb9c9339effb8754736d5048f951
Melih Güleç <melih.gulec14@gmail.com>
2023-03-22 11:47:11 +0300

    Create product categories table

---
+++ app/Tables/ProductCategoriesTable.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Tables;
+
+use App\Models\ProductCategory;
+use Okipa\LaravelTable\Abstracts\AbstractTableConfiguration;
+use Okipa\LaravelTable\Column;
+use Okipa\LaravelTable\Formatters\DateFormatter;
+use Okipa\LaravelTable\RowActions\DestroyRowAction;
+use Okipa\LaravelTable\RowActions\EditRowAction;
+use Okipa\LaravelTable\Table;
+
+class ProductCategoriesTable extends AbstractTableConfiguration
+{
+    protected function table(): Table
+    {
+        return Table::make()->model(ProductCategory::class)
+            ->rowActions(fn(ProductCategory $productCategory) => [
+                new DestroyRowAction(),
+            ]);
+    }
+
+    protected function columns(): array
+    {
+        return [
+            Column::make('id')->sortable()->title('ID'),
+            Column::make('name')->sortable()->title('Name'),
+            Column::make('parent')->format(function($query){
+                return $query->parent->name ?? '---';
+            })->title('Parent Category'),
+            Column::make('created_at')->format(new DateFormatter('d/m/Y H:i'))->sortable()->title('Created At'),
+            Column::make('updated_at')->format(new DateFormatter('d/m/Y H:i'))->sortable()->sortByDefault('desc')->title('Updated At'),
+        ];
+    }
+
+    protected function results(): array
+    {
+        return [
+            // The table results configuration.
+            // As results are optional on tables, you may delete this method if you do not use it.
+        ];
+    }
+}
