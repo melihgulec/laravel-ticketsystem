399252f633157be65463b6ccaf6fb01e3feae994
Melih Güleç <melih.gulec14@gmail.com>
2023-03-22 15:57:19 +0300

    Add icon to infobar at staff dashboard page

--- resources/views/staff/create.blade.php
+++ resources/views/staff/create.blade.php
@@ -10,16 +10,16 @@
         </div>
         <h1 class="text-2xl font-medium mt-12">Statuses</h1>
         <div class="grid grid-cols-3 gap-x-6">
-            <x-info-bar title="Tickets with open status" subtitle="{{ $ticketsWithOpenStatusCount }}"/>
-            <x-info-bar title="Tickets with replied status" subtitle="{{ $ticketsWithRepliedStatusCount }}"/>
-            <x-info-bar title="Tickets with closed status" subtitle="{{ $ticketsWithClosedStatusCount }}"/>
+            <x-info-bar title="Tickets with open status" subtitle="{{ $ticketsWithOpenStatusCount }}" icon="lock-open"/>
+            <x-info-bar title="Tickets with replied status" subtitle="{{ $ticketsWithRepliedStatusCount }}" icon="message-dots"/>
+            <x-info-bar title="Tickets with closed status" subtitle="{{ $ticketsWithClosedStatusCount }}" icon="lock"/>
         </div>
         <h1 class="text-2xl font-medium mt-12">Priorities</h1>
         <div class="grid grid-cols-4 gap-x-6">
-            <x-info-bar title="Tickets with critical priority" subtitle="{{ $ticketsWithCriticalPriorityCount }}"/>
-            <x-info-bar title="Tickets with high priority" subtitle="{{ $ticketsWithHighPriorityCount }}"/>
-            <x-info-bar title="Tickets with medium priority" subtitle="{{ $ticketsWithMediumPriorityCount }}"/>
-            <x-info-bar title="Tickets with low priority" subtitle="{{ $ticketsWithLowPriorityCount }}"/>
+            <x-info-bar title="Tickets with critical priority" subtitle="{{ $ticketsWithCriticalPriorityCount }}" icon="arrow-badge-up-filled"/>
+            <x-info-bar title="Tickets with high priority" subtitle="{{ $ticketsWithHighPriorityCount }}" icon="arrow-narrow-up"/>
+            <x-info-bar title="Tickets with medium priority" subtitle="{{ $ticketsWithMediumPriorityCount }}" icon="arrow-narrow-right"/>
+            <x-info-bar title="Tickets with low priority" subtitle="{{ $ticketsWithLowPriorityCount }}" icon="arrow-narrow-down"/>
         </div>
     </div>
 </x-layout>
