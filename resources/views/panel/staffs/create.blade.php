02db0f901b99da94a8367dd7e50f087847290507
Melih Güleç <melih.gulec14@gmail.com>
2023-03-27 14:34:08 +0300

    Add user add route to link at staffs page

--- resources/views/panel/staffs/create.blade.php
+++ resources/views/panel/staffs/create.blade.php
@@ -5,7 +5,7 @@
         subtitle="Be careful! This item will be deleted forever!" />
     <div class="flex flex-col">
         <div class="flex justify-end">
-            <a href="#" class="px-4 py-2 bg-blue-500 rounded-md transition-all duration-100 hover:bg-blue-600 w-24 flex flex-row items-center justify-center text-white mb-4">
+            <a href="/admin/users/add" class="px-4 py-2 bg-blue-500 rounded-md transition-all duration-100 hover:bg-blue-600 w-24 flex flex-row items-center justify-center text-white mb-4">
                 <x-tabler-icon-svg svg="plus" />
                 Add
             </a>
